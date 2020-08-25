60e933010127d2bb6c5607533f47a621df9d8c8a
MuktiPaliwal <65341949+MuktiPaliwal@users.noreply.github.com>
2020-08-25 19:39:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup(){
+
+}
+function draw(){
+
+}
+function openAttachment() {
+    document.getElementById('attachment').click();
+  }
+  
+  function fileSelected(input){
+    document.getElementById('btnAttachment').value = "File: " + input.files[0].name
+  }
